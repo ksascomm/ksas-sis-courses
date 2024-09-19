ac6145eb9d46e94635185adba2de88fb17dc2265
ksascomm <ksasweb@jhu.edu>
2024-09-19 10:56:45 -0400

    Update DataTables & extensions

--- ksas-sis-courses.php
+++ ksas-sis-courses.php
@@ -32,11 +32,11 @@ if ( ! function_exists( 'register_script' ) ) {
 	 * @since 1.0.0
 	 */
 	function register_script() {
-		wp_enqueue_style( 'data-tables', '//cdn.datatables.net/2.1.4/css/dataTables.dataTables.min.css', array(), true );
+		wp_enqueue_style( 'data-tables', '//cdn.datatables.net/2.1.6/css/dataTables.dataTables.min.css', array(), true );
 
 		wp_enqueue_style( 'data-tables-searchpanes', '//cdn.datatables.net/searchpanes/2.3.2/css/searchPanes.dataTables.min.css', array(), true );
 
-		wp_enqueue_style( 'data-tables-responsive', '//cdn.datatables.net/responsive/3.0.2/css/responsive.dataTables.min.css', array(), true );
+		wp_enqueue_style( 'data-tables-responsive', '//cdn.datatables.net/responsive/3.0.3/css/responsive.dataTables.min.css', array(), true );
 
 		wp_register_style( 'courses-css', plugins_url( '/css/courses.css', __FILE__ ), false, '1.0.4', 'all' );
 
@@ -46,10 +46,10 @@ if ( ! function_exists( 'register_script' ) ) {
 		wp_enqueue_script( 'data-tables-searchpanes', '//cdn.datatables.net/searchpanes/2.3.2/js/dataTables.searchPanes.min.js', array(), '2.3.2', false );
 		wp_script_add_data( 'data-tables-searchpanes', 'defer', true );
 
-		wp_enqueue_script( 'data-tables-select', '//cdn.datatables.net/select/2.0.5/js/dataTables.select.min.js', array(), '2.0.5', false );
+		wp_enqueue_script( 'data-tables-select', '//cdn.datatables.net/select/2.1.0/js/dataTables.select.min.js', array(), '2.1.0', false );
 		wp_script_add_data( 'data-tables-select', 'defer', true );
 
-		wp_enqueue_script( 'data-tables-responsive', '//cdn.datatables.net/responsive/3.0.2/js/dataTables.responsive.min.js', array(), '3.0.2', false );
+		wp_enqueue_script( 'data-tables-responsive', '//cdn.datatables.net/responsive/3.0.3/js/dataTables.responsive.min.js', array(), '3.0.3', false );
 		wp_script_add_data( 'data-tables-responsive', 'defer', true );
 
 		wp_register_script( 'courses-js', plugins_url( '/js/courses.js', __FILE__ ), array( 'jquery' ), '1.0.2', true );
